ed1b06232e1eb975eed58d1988983661d41e1598
Alex Stone <mcastone5@gmail.com>
2022-04-10 14:39:10 -0400

    Adds gitignore and now code works

--- bot.py
+++ bot.py
@@ -1,19 +1,16 @@
-import discord
+# bot.py
 import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f'{client.user} has connected to Discord!')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-client.run(os.getenv('OTYyNzcyMzYyMDc0NDAyODM2.YlMZog.dkTYSLEOPAo1UZ-ygv_Xtk5Od6s'))
-
+client.run(TOKEN)
