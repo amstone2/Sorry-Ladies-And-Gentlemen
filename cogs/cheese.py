5bd1ebb777fcd85844726fb2095ef1a779c24ce9
Ryan <haasryanj@gmail.com>
2022-04-15 01:02:01 -0400

    cleanup and change formatting for writting to JSON

--- cogs/cheese.py
+++ cogs/cheese.py
@@ -1,3 +1,4 @@
+# Imports
 from ast import Return
 from discord.ext import commands
 import random
@@ -8,66 +9,45 @@ from discord.ext import tasks
 from os.path import exists
 import json
 
-
-activateTouchGlobal = True
-infectionRateGlobal = 5.00
-
-
-
 class cheese(commands.Cog):
+
     def __init__(self, bot):
         self.bot = bot
-    global infectionRateGlobal
-
-
+        self.activateTouchGlobal = True
+        self.infectionRateGlobal = 5.00
 
     @commands.Cog.listener()
     async def on_ready(self):
         self.change_presence.start()
+                
         if exists('ct_users.json') == False:
+            print('ct_users.json not found')
+
             t = time.time()
 
-            
-            connected_guilds = []
-            for guild in self.bot.guilds:
-                connected_guilds.append(guild.name)
+            data = {}
+            for server in self.bot.guilds:
+                cheese_role = discord.utils.get(server.roles, name="Cheese Touch")
+                if not cheese_role:
+                    # self.assign_cheese()
+                    print('need to assign person with cheese touch')
+                    continue
+                person_with_cheese = cheese_role.members[0]
+                data[server.name] = {'name': person_with_cheese.name, 'id': person_with_cheese.id, 'timestamp': t}
+            print(data)
 
-
-            cheeseTouchUsers =  []
-            for guild in self.bot.guilds:
-                for user in guild.members:
-                    cheeseTouch = discord.utils.get(guild.roles, name="Cheese Touch")
-                    if cheeseTouch in user.roles:
-                        cheeseTouchUsers.append(user.name)
-            dic = {}
-            for i in range(len(connected_guilds)):
-                dic[connected_guilds[i]] = [cheeseTouchUsers[i], t]
-            
-            print(dic)
-
-            print("Creating new file")
-            jsonString = json.dumps(dic)
+            print("Creating ct_users.json")
+            jsonString = json.dumps(data)
             jsonFile = open("ct_users.json", "w")
             jsonFile.write(jsonString)
             jsonFile.close()
         else:
             with open('ct_users.json') as json_file:
-                data = json.load(json_file)
-
-
-        all_guilds = []
-        for guild in self.bot.guilds:
-            all_guilds.append(guild)
-
-
-        for channel in all_guilds[0].channels:
-            print(channel)
+                self.users_with_cheese = json.load(json_file)
 
     @tasks.loop(minutes=1) 
     async def change_presence(self):
         return
-
-
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -76,16 +56,13 @@ class cheese(commands.Cog):
         if message.author.bot:
             return
 
-        if (activateTouchGlobal == False):
+        if (self.activateTouchGlobal == False):
             return
         
         # if(message.guild.name == 'Ligma Pi Fraternity'):
         #     return 
         
-
         cheeseTouch = discord.utils.get(message.guild.roles, name="Cheese Touch")
-
-
 
         tenMostRecentAuthors = []
         tenMessages = await message.channel.history(limit=10).flatten()
@@ -104,11 +81,9 @@ class cheese(commands.Cog):
             if cheeseTouch in user.roles:
                 personWithCheeseTouch = user
 
-
         if personWithCheeseTouch in tenMostRecentAuthors:
             print("Cheese touch user within most recent authors \n")
             tenMostRecentAuthors.remove(personWithCheeseTouch)
-
 
             print("Ten most recent authors no cheese touch")
             for author in tenMostRecentAuthors:
@@ -117,9 +92,9 @@ class cheese(commands.Cog):
 
             randomFloat = round(random.uniform(0.00, 100.00), 2)
             print("Random float: ", randomFloat)
-            print("Percent chance: ", infectionRateGlobal)
+            print("Percent chance: ", self.infectionRateGlobal)
             print("\n")
-            if randomFloat < float(infectionRateGlobal):
+            if randomFloat < float(self.infectionRateGlobal):
                 if(len(tenMostRecentAuthors) > 0):
                     await message.channel.send(f"{personWithCheeseTouch.mention} No longer has the cheese touch!")
                     await personWithCheeseTouch.remove_roles(cheeseTouch)
@@ -132,8 +107,6 @@ class cheese(commands.Cog):
                     await author.add_roles(cheeseTouch)
                     await message.channel.send(f"<@{author.id}> Now has the cheese touch! Typing close to other users will have a chance to give it to them.")
                     await message.channel.send(f"https://tenor.com/view/cheese-touch-diary-of-a-wimpy-kid-greg-no-gif-25045298")
-            
-
     
     @commands.command(
 		name='changeInfectionRate',
@@ -141,14 +114,12 @@ class cheese(commands.Cog):
 		usage='changeInfectionRate'
 	)
     async def changeInfectionRate(self, ctx, arg):
-        global infectionRateGlobal
         authorID = str(ctx.author.id)
         if(authorID == "163862875738341376"):
             print("Alex sent this command")
-            oldInfectionRate = infectionRateGlobal
-            infectionRateGlobal = arg
-            await ctx.channel.send(f"<@{ctx.author.id}> Changed infection rate from {oldInfectionRate} to {infectionRateGlobal} percent.")
-        
+            oldInfectionRate = self.infectionRateGlobal
+            self.infectionRateGlobal = arg
+            await ctx.channel.send(f"<@{ctx.author.id}> Changed infection rate from {oldInfectionRate} to {self.infectionRateGlobal} percent.")
 
     @commands.command(
 		name='changeActivationStatus',
@@ -156,14 +127,11 @@ class cheese(commands.Cog):
 		usage='whoIsInfected'
 	)
     async def changeActivationStatus(self, ctx, arg):
-        global activateTouchGlobal
         authorID = str(ctx.author.id)
         if(authorID == "163862875738341376"):
-            oldTouch = activateTouchGlobal
-            activateTouchGlobal = arg
+            oldTouch = self.activateTouchGlobal
+            self.activateTouchGlobal = arg
             await ctx.channel.send(f"{ctx.author.mention} Changed activation rate from {oldTouch} to {arg}.")
-
-
 
     @commands.command(
 		name='whoIsInfected',
@@ -179,7 +147,6 @@ class cheese(commands.Cog):
                 await ctx.channel.send(f"{user.mention} Is infected with the cheese touch.")
 
         await ctx.channel.send('https://tenor.com/view/thats-gross-ewww-disgusting-gif-14383467')
-        
 
 def setup(bot):
     bot.add_cog(cheese(bot))
